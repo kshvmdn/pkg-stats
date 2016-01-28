44314387adfeeb44dbe4d0b0922ba3b6ea7a634c
Kashav Madan <kshvmdn@gmail.com>
2016-01-28 17:10:55 -0500

    Add support for package names, styled output

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 const Promise = require('pinkie-promise');
 const Xray = require('x-ray'), xray = Xray();
 const got = require('got');
-const cli = require('./cli');
+const cli = require('./cli').flags;
 const colors = require('colors');
 const _ = require('lodash');
 
 var getUsrPkgs = function(username) {
   return new Promise(function(resolve, reject) {
+
+    if (username == '') {
+      resolve([cli.p]);
+    }
+
     var url = 'https://www.npmjs.com/~' + username + '/';
     xray(url, '.collaborated-packages', ['li'], 'a')(function(err, pkgs) {
       if (err) reject(err);
@@ -18,36 +23,45 @@ var getUsrPkgs = function(username) {
       });
       resolve(usrPkgs);
     });
+
   });
 }
 
 var getPkgStats = function(pkgs) {
   return new Promise(function(resolve, reject) {
+
     var base = 'https://api.npmjs.org/downloads/point/';
-    var period = 'last-' + cli.flags['t'] + '/';
+    var period = 'last-' + cli.t + '/';
     var packages = pkgs.length > 1 ? pkgs.join() : String(pkgs);
-    got(base + period + packages, {json: true})
+    got(base + period + packages, { json: true })
       .then(response => {
         resolve(response.body);
       })
       .catch(error => {
         reject(error);
       });
+
   });
 }
 
 var parseJson = function(pkgs) {
   return new Promise(function(resolve, reject) {
     var output = '';
-    // console.log(pkgs);
-    _.forOwn(pkgs, function(pkg, pkgName) {
-      output += pkgName + ' - ' + pkg.downloads + ' downloads\n'
-    })
+    if (cli.p == undefined) {
+      output += colors.white( 'Package download stats for user ' + (cli.u).magenta + ' in the last ' + (cli.t).magenta + ':\n\n' );
+      _.forOwn(pkgs, function(pkg, key) {
+        output += colors.white(' - ' + (pkg.package).cyan + ' downloaded ' + String(pkg.downloads).cyan + ' times\n');
+      });
+    } else {
+      output = colors.white('Package ' + (pkgs.package).cyan + ' was downloaded ' + String(pkgs.downloads).cyan + ' times in the last ' + (cli.t).cyan + '.');
+    }
     resolve(output.trim());
   });
 }
 
-getUsrPkgs(cli.flags['u'])
+var user = cli.u != undefined ? cli.u : '';
+
+getUsrPkgs(user)
   .then(pkgs => {
     return getPkgStats(pkgs);
   })
@@ -56,8 +70,9 @@ getUsrPkgs(cli.flags['u'])
   })
   .then(output => {
     console.log(output);
+    process.exit(0);
   })
   .catch(e => {
-    console.log(e);
+    console.log(e.red);
     process.exit(1);
   });
