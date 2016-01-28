f6a48dfd9e61dae068f40c32988f5ce6882693bd
Kashav Madan <kshvmdn@gmail.com>
2016-01-28 16:22:07 -0500

    Add getter for pkg. stats, basic output

--- index.js
+++ index.js
@@ -2,16 +2,18 @@
 'use strict';
 const Promise = require('pinkie-promise');
 const Xray = require('x-ray'), xray = Xray();
+const got = require('got');
 const cli = require('./cli');
+const colors = require('colors');
+const _ = require('lodash');
 
-const getUsrPkgs = function(username) {
+var getUsrPkgs = function(username) {
   return new Promise(function(resolve, reject) {
     var url = 'https://www.npmjs.com/~' + username + '/';
     xray(url, '.collaborated-packages', ['li'], 'a')(function(err, pkgs) {
       if (err) reject(err);
-      // resolve(data);
       var usrPkgs = [];
-      pkgs.forEach(function(pkg) {
+      _.each(pkgs, function(pkg) {
         usrPkgs.push(pkg.trim().split('- ')[0].trim());
       });
       resolve(usrPkgs);
@@ -19,6 +21,43 @@ const getUsrPkgs = function(username) {
   });
 }
 
-getUsrPkgs(cli.flags['u']).then(function(pkgs) {
-  return pkgs;
-});
+var getPkgStats = function(pkgs) {
+  return new Promise(function(resolve, reject) {
+    var base = 'https://api.npmjs.org/downloads/point/';
+    var period = 'last-' + cli.flags['t'] + '/';
+    var packages = pkgs.length > 1 ? pkgs.join() : String(pkgs);
+    got(base + period + packages, {json: true})
+      .then(response => {
+        resolve(response.body);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+
+var parseJson = function(pkgs) {
+  return new Promise(function(resolve, reject) {
+    var output = '';
+    // console.log(pkgs);
+    _.forOwn(pkgs, function(pkg, pkgName) {
+      output += pkgName + ' - ' + pkg.downloads + ' downloads\n'
+    })
+    resolve(output.trim());
+  });
+}
+
+getUsrPkgs(cli.flags['u'])
+  .then(pkgs => {
+    return getPkgStats(pkgs);
+  })
+  .then(pkgStats => {
+    return parseJson(pkgStats);
+  })
+  .then(output => {
+    console.log(output);
+  })
+  .catch(e => {
+    console.log(e);
+    process.exit(1);
+  });
